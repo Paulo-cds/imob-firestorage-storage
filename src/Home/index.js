ff945e80f9862cb51ca08bc0768899a244e08d55
paulo-cds <paulo.pdomingues@hotmail.com>
2022-02-09 10:42:14 -0300

    inserindo botao para pagina ListData

--- src/Home/index.js
+++ src/Home/index.js
@@ -8,6 +8,7 @@ const Home = () => {
         <div className='homeContainer'>            
             <button onClick={() => navigate(`storage`)}>Storage</button>
             <button onClick={() => navigate(`firestore`)}>Firestore</button>
+            <button onClick={() => navigate(`/ListData`)}>Lista Completa</button>
         </div>
     )
 }
