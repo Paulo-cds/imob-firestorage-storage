84357d2aae936a2653444045df99ea646e058d80
paulo-cds <paulo.pdomingues@hotmail.com>
2022-02-13 10:53:24 -0300

    atualizacao form e insercao modal no table

--- src/Components/TableGrid/index.js
+++ src/Components/TableGrid/index.js
@@ -1,27 +1,58 @@
-import { useState, useEffect } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import {db} from '../../firestore.js'
+import Card from '../Card'
+import { useState } from 'react';
+import './tableStyle.scss'
+
 
 
 export default function DataTable({data,columns}) {    
+  const [cardComplet, setCardComplet] = useState(false)
+  const [itemsCard, setItemsCard] = useState([])
+
+  const handleSetCard = e => {
+    if(e.row.id === itemsCard.id && cardComplet === true){
+      setCardComplet(false)
+    }else{
+      setItemsCard(e.row)
+      setCardComplet(true)
+    }
+    
+  }
 
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-        rows={data.map((proprietario, index) => {
-          return {
-            id: index,
-            proprietario: proprietario.proprietario,
-            inquilino: proprietario.inquilino,
-            imovel: proprietario.imovel,
-            contrato: proprietario.contrato,
-          }
-        })}
-      />
+    <div className='containerTable'>
+      <div style={{ height: 400, width: '90%' }}>
+        <DataGrid
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          checkboxSelection
+          onRowClick={(e)=>handleSetCard(e)}
+          rows={data.map((proprietario, index) => {
+            return {
+              id: index,
+              proprietario: proprietario.proprietario,
+              inquilino: proprietario.inquilino,
+              imovel: proprietario.imovel,
+              contrato: proprietario.contrato,
+            }
+          })}        
+        />
+      </div>
+      <div className='Card' style={{display : cardComplet ? 'flex' : 'none'}}>
+        <Card
+          proprietario={itemsCard.proprietario}
+          inquilino={itemsCard.inquilino}
+          imovel={itemsCard.imovel}
+          contrato={itemsCard.contrato}
+          // id={propId[index]}              
+          //handleDelete={handleDelete}
+          // key = {index}
+        /> 
+        <p className='Close' onClick={()=>setCardComplet(false)}>Fechar</p>
+      
+      </div>
+
     </div>
   );
 }
